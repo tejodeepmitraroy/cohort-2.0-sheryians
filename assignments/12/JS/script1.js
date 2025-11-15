3a798a98d6bc96e154c5ffbe23d985f9cbf72130
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2025-11-15 23:40:11 +0530

    feat: add assignment 12

---
+++ assignments/12/JS/script1.js
@@ -0,0 +1,71 @@
+// Task 1
+
+// alert("Hello from Sheryians!");
+
+/////////////////////////////////////////////
+// Task 4
+// var a = " Harsh "
+// var b = " Sheryians "
+// const c = " School "
+
+// a = "Updated";
+// b="Updated"
+
+/////////////////////////////////////////////
+// Task 5
+// 1)
+// console.log("Console.log -->","Tejodeep","22","Kolkata")
+// console.info("Console.info -->","Tejodeep","22","Kolkata")
+// console.warn("Console.warn -->","Tejodeep","22","Kolkata")
+// console.error("Console.error -->","Tejodeep","22","Kolkata")
+
+// 2)
+// let user = prompt("Enter Yourname");
+// alert(`welcome ${user}`)
+
+// 3)
+// console.log(typeof user );
+
+// 4)
+// let age = prompt("Enter age");
+// console.log(`${age + 5}`);
+
+///////////////////////////////////////////////
+// Task 6
+// let msg = "I love Sheryians";
+
+// console.log("Message slice-->", msg.slice(2, 6));
+// console.log("Message split-->", msg.split(" "));
+// console.log("Message Replace-->", msg.replace("love", "study at"));
+
+// let name = "Hash"
+// console.log(`Hey ${name}, Welcome in JS`);
+
+// console.log(msg.includes("love"))
+
+///////////////////////////////////////////////
+// Task 9
+
+// let age = 25;
+// let name = "Harsh";
+// let isStudent = true;
+// let skills = ["JS", "HTML"];
+// let user = { city: "Bhopal" };
+// let x = null;
+// let y;
+// let z = Symbol("id");
+
+
+// console.log(typeof age);
+// console.log(typeof name)
+// console.log(typeof isStudent)
+// console.log(typeof skills)
+// console.log(typeof user)
+// console.log(typeof x)
+// console.log(typeof y)
+// console.log(typeof z)
+
+//////////////////////////////////////////////////////
+
+
+
